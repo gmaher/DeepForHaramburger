08be0b4ebe58f647c1df851985a71d7e75773ddc
Nikhil <nikparth@gmail.com>
2016-11-12 02:05:54 +0000

    add bedfile api framework and test, add conda install file

--- api/test.py
+++ api/test.py
@@ -1,8 +1,23 @@
 import fasta
+from BedFile import BedFile
+from pybedtools import BedTool, example_filename
 
-api = fasta.fasta_api('/home/marsdenlab/datasets/seq2seq-data/hg38.fa')
-chr_list = ['chr3','chr4','chr5','chr6','chr7']
-start_list = [10000, 50000, 100000, 4, 250000]
-end_list = [11000, 51000, 101000,1004, 251000]
-x = api.get_batch(chr_list,start_list,end_list)
-print x.shape
+#BedFile test
+reference_genome = 'hg19'
+bedfilename = '/data/seq2seq-data/GM12878_cells/peak/macs2/overlap/E116.GM12878_Lymphoblastoid_Cells.ENCODE.Duke_Crawford.DNase-seq.merged.20bp.filt.50m.pf.pval0.1.500000.naive_overlap.narrowPeak.gz'
+flank_length = 1000
+bedobj = BedFile(bedfilename, reference_genome, flank_length)
+chr_list, start_list, end_list = bedobj.extractIntervals()
+print(chr_list[0], start_list[0], end_list[0])
+
+#use pybedtools to read in batches from the fasta_file
+fasta_fn = '/data/seq2seq-data/' + reference_genome + '.fa'
+batch_size = 10
+batch_bed = BedTool(bedobj.slopped[0:batch_size])
+batched_fasta = open(batch_bed.sequence(fi = example_filename(fasta_fn)).seqfn).read()
+print(batched_fasta)
+
+#fasta_api SeqIO is too slow -- need to fix, or use above pybedtools reader
+#api = fasta.fasta_api('/data/seq2seq-data/'+ reference_genome +'.fa')
+#x = api.get_batch(chr_list[0:128],start_list[0:128],end_list[0:128])
+#print(x.shape)
---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+conda install -c bioconda pybedtools
+conda install -c bioconda bedtools
+conda install -c bioconda htslib
+conda install -c bioconda biopython 
