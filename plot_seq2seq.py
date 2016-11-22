3a97c47c208375d90ffccd0990f6d09e25ea8c16
gabriel <gmaher2@hotmail.com>
2016-11-22 13:33:18 -0800

    script for plotting DHS

---
+++ plot_seq2seq.py
@@ -0,0 +1,42 @@
+import sys
+import os
+sys.path.append(os.path.abspath('./api'))
+print(sys.path)
+
+from api.data import DataReader
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('seq2seq_dir')
+args = parser.parse_args()
+
+dataDir = args.seq2seq_dir
+
+bedfilename = (dataDir+"GM12878_cells/peak/macs2/overlap/"
+                "E116.GM12878_Lymphoblastoid_Cells.ENCODE.Duke_Crawford."
+                "DNase-seq.merged.20bp.filt.50m.pf.pval0.1.500000."
+                "naive_overlap.narrowPeak.gz")
+referenceGenome = 'hg19'
+flankLength = 400
+fastaFname = dataDir+"hg19.fa"
+bigwigFname = (dataDir+"GM12878_cells/signal/macs2/rep1/"
+                "E116.GM12878_Lymphoblastoid_Cells.ENCODE.Duke_Crawford."
+                "DNase-seq.merged.20bp.filt.50m.pf.fc.signal.bigwig")
+
+reader = DataReader(bedfilename, referenceGenome, flankLength, fastaFname,
+        bigwigFname)
+
+X,y = reader.getBatch(10000)
+
+print(X.shape)
+print(y.shape)
+
+plt.figure()
+plt.hist(np.ravel(y),bins=50)
+plt.show()
+
+plt.figure()
+plt.plot(y[:10].T)
+plt.show()
