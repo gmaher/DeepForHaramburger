5fbd504638cbc10a4064a6be93e756c7ac19e12d
marsdenlab <gmaher2@hotmail.com>
2016-12-11 11:29:46 -0800

    updated fcn

--- run.sh
+++ run.sh
@@ -11,3 +11,13 @@ py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run4.cfg
 py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run5.cfg
 
 py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run6.cfg
+
+py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run7.cfg
+
+py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run8.cfg
+
+py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run9.cfg
+
+py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run10.cfg
+
+py3 fcn.py /home/marsdenlab/datasets/seq2seq-data/ ./run11.cfg
